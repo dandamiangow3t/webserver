63ab34c3d0b346ff2fd89ae5e5d67fe14fdd7c6e
Dan Damian <arcassis@gmail.com>
2024-11-11 18:23:07 +0200

    We have now a simple web server.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
-use std::net::TcpListener;
+use std::fs;
+use std::io::{prelude::*, BufReader};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buff_reader = BufReader::new(&mut stream);
+    let request_line = buff_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+    let response = format!(
+        "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+    );
+    stream.write_all(response.as_bytes()).unwrap();
 }
